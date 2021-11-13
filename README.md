742098cb051188c64e2ad33e8379e629b0310a7e
Reshab <mehtareshab@gmail.com>
2021-11-12 23:26:56 -0800

    Finished setting up npm and step1 of expose

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lab 8 - Starter
+ 
+Authored By : Reshab Mehta and Guy
+ 
+1) I would you fit automated tests within a Github action that runs whenever code is pushed. I would do this to prevent any bad push breaking the app and it firther ensures test are run and working.
+ 
+2) No
--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -23,19 +23,23 @@ describe('Basic user flow for Website', () => {
     let data, plainValue;
     // Query select all of the <product-item> elements
     const prodItems = await page.$$('product-item');
-    console.log(`Checking product item 1/${prodItems.length}`);
-    // Grab the .data property of <product-items> to grab all of the json data stored inside
-    data = await prodItems[0].getProperty('data');
-    // Convert that property to JSON
-    plainValue = await data.jsonValue();
-    // Make sure the title, price, and image are populated in the JSON
-    if (plainValue.title.length == 0) { allArePopulated = false; }
-    if (plainValue.price.length == 0) { allArePopulated = false; }
-    if (plainValue.image.length == 0) { allArePopulated = false; }
-    // Expect allArePopulated to still be true
-    expect(allArePopulated).toBe(true);
 
-    // TODO - Step 1
+    for (let i = 0; i < prodItems.length; i++) {
+      console.log(`Checking product item ${i + 1}/${prodItems.length}`);
+      // Grab the .data property of <product-items> to grab all of the json data stored inside
+      data = await prodItems[1].getProperty('data');
+      // Convert that property to JSON
+      plainValue = await data.jsonValue();
+      // Make sure the title, price, and image are populated in the JSON
+      if (plainValue.title.length == 0) { allArePopulated = false; }
+      if (plainValue.price.length == 0) { allArePopulated = false; }
+      if (plainValue.image.length == 0) { allArePopulated = false; }
+      // Expect allArePopulated to still be true
+      expect(allArePopulated).toBe(true);
+
+    }
+
+    // TODO - Step 1 (DONE)
     // Right now this function is only checking the first <product-item> it found, make it so that
     // it checks every <product-item> it found
 
