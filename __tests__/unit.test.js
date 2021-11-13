4df59d35fce39988def2c77c6c5c36fea9b9d07a
Reshab <mehtareshab@gmail.com>
2021-11-13 13:51:43 -0800

    Finished the remaining unit tests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -16,22 +16,75 @@ test('test isPhoneNumber 3', () => {
 	expect(functions.isPhoneNumber('510 - 75588741')).toBe(false);
 });
 
-test('test isPhoneNumber 3', () => {
+test('test isPhoneNumber 4', () => {
 	expect(functions.isPhoneNumber('(015) 758 -8741')).toBe(false);
 });
 
-test('isEmail 1', () => {
+
+test('test isEmail 1', () => {
 	expect(functions.isEmail('you@example.com')).toBe(true);
 });
 
-test('isEmail 2', () => {
+test('test isEmail 2', () => {
 	expect(functions.isEmail('michaelscott@ucsd.edu')).toBe(true);
 });
 
-test('isEmail 3', () => {
+test('test isEmail 3', () => {
 	expect(functions.isEmail('JerrySeinfeld@kr@mer.net')).toBe(false);
 });
 
-test('isEmail 4', () => {
+test('test isEmail 4', () => {
 	expect(functions.isEmail('billy Joel.com')).toBe(false);
 });
+
+
+test('test isHexColor 1', () => {
+	expect(functions.isHexColor('#E9E098')).toBe(true);
+});
+
+test('test isHexColor 2', () => {
+	expect(functions.isHexColor('#000000')).toBe(true);
+});
+
+test('test isHexColor 3', () => {
+	expect(functions.isHexColor('#E9E09890')).toBe(false);
+});
+
+test('test isHexColor 4', () => {
+	expect(functions.isHexColor('#color')).toBe(false);
+});
+
+
+test('test isDate 1', () => {
+	expect(functions.isDate('11/13/2021')).toBe(true);
+});
+
+test('test isDate 2', () => {
+	expect(functions.isDate('5/13/2019')).toBe(true);
+});
+
+test('test isDate 3', () => {
+	expect(functions.isDate('011/05/2021')).toBe(false);
+});
+
+test('test isDate 4', () => {
+	expect(functions.isDate('04/AA/2020')).toBe(false);
+});
+
+
+test('test isStrongPassword 1', () => {
+	expect(functions.isStrongPassword('Apple_33')).toBe(true);
+});
+
+test('test isStrongPassword 2', () => {
+	expect(functions.isStrongPassword('CSE110lab8_')).toBe(true);
+});
+
+test('test isStrongPassword 3', () => {
+	expect(functions.isStrongPassword('123')).toBe(false);
+});
+
+test('test isStrongPassword 4', () => {
+	expect(functions.isStrongPassword(' ')).toBe(false);
+});
+
